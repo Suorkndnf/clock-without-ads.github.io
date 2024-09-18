38f6747f427b77b9272d59b099c883a6b6001ff8
Suorndnf <133608569+Suorkndnf@users.noreply.github.com>
2024-09-18 21:49:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function actualizarReloj() {
+    const reloj = document.getElementById("reloj");
+    const ahora = new Date();
+    const horas = ahora.getHours().toString().padStart(2, '0');
+    const minutos = ahora.getMinutes().toString().padStart(2, '0');
+    const segundos = ahora.getSeconds().toString().padStart(2, '0');
+    
+    reloj.textContent = `${horas}:${minutos}:${segundos}`;
+}
+
+// Actualizar el reloj cada segundo
+setInterval(actualizarReloj, 1000);
+
+// Iniciar el reloj al cargar la página
+actualizarReloj();
